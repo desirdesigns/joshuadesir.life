edac387394c079d36c51697c5dd9e77d14ab02b5
Jeffrey Desir <jeffreyjdesir@pm.me>
2021-12-08 01:07:16 -0500

    [ summayr concluded ]

--- DOCUMENTATION.md
+++ DOCUMENTATION.md
@@ -1,2 +1,8 @@
 (load:docs/TITLE.md)
+(load:docs/LOGO.md)
+(load:docs/SCREENSHOTS)
+(load:docs/TL;DR.md)
+(load:docs/TECHNOLOGIES)
+(load:docs/USAGE.md)
 (load:docs/LICENSE.md)
+
--- app/assets/scripts/app/main.js
+++ app/assets/scripts/app/main.js
@@ -250,7 +250,7 @@
 					return s;
 
 				},
-				fadeSpeed: 300,
+				fadeSpeed: 500,
 				onPopupClose: function() { $body.removeClass('modal-active'); },
 				onPopupOpen: function() { $body.addClass('modal-active'); },
 				overlayOpacity: 0,
